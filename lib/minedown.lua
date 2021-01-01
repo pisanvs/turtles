eef9f64eef3f2a08031783b734cf1ea10189be72
Maximiliano Morel <maxmorel@pisanvs.cl>
2021-01-01 19:31:15 -0300

    first pog

---
+++ lib/minedown.lua
@@ -0,0 +1,1 @@
+return ('minedown called')
---
+++ startup.lua
@@ -0,0 +1,17 @@
+require('lib')
+local functionTable = {lib.minedown}
+
+while turtle.getFuelLevel() < 2000 do
+    print('waiting for refuel, ',turtle.getFuelLevel(), ' out of 100')
+    turtle.refuel(1)
+end
+
+print('am done')
+print('enter command')
+
+local input = read()
+if functionTable[input] then
+    functionTable[input]()
+else
+    print("I don't know how to "..input.."!")
+end
